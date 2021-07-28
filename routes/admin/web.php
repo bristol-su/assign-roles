b1edd687e8fa35733d6f486ff3fd1039d0c3a3fb
Toby Twigger <tobytwigger1@gmail.com>
2021-07-28 12:24:09 +0100

    Enable sdk v5

--- routes/admin/web.php
+++ routes/admin/web.php
@@ -14,5 +14,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware([\BristolSU\Module\AssignRoles\Http\Middleware\BindRoleRepository::class])->group(function() {
-    Route::get('/', 'AdminPageController@index');
+    Route::get('/', [\BristolSU\Module\AssignRoles\Http\Controllers\AdminPageController::class, 'index']);
 });
--- routes/participant/api.php
+++ routes/participant/api.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::namespace('ParticipantApi')->middleware(\BristolSU\Module\AssignRoles\Http\Middleware\BindRoleRepository::class)->group(function() {
-    Route::resource('role', 'RoleController')->only('index', 'store', 'destroy', 'update');
-    
+Route::middleware(\BristolSU\Module\AssignRoles\Http\Middleware\BindRoleRepository::class)->group(function() {
+    Route::resource('role', \BristolSU\Module\AssignRoles\Http\Controllers\ParticipantApi\RoleController::class)->only('index', 'store', 'destroy', 'update');
+
     Route::prefix('role/{role}')->group(function() {
-        Route::resource('user', 'AssignController')->only('update', 'destroy');
+        Route::resource('user', \BristolSU\Module\AssignRoles\Http\Controllers\ParticipantApi\AssignController::class)->only('update', 'destroy');
     });
-    Route::resource('members', 'MemberController')->only('index');
+    Route::resource('members', \BristolSU\Module\AssignRoles\Http\Controllers\ParticipantApi\MemberController::class)->only('index');
 });
